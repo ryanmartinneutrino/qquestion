dbfb63be7b1d25e1380c61e9c82e15d02b5a3b9c
Ryan Martin <ryanmartinneutrino@gmaill.com>
2017-04-15 18:21:55 -0400

    styled delete button, added edit button, but needs logic in router

--- imports/ui/QuestionPreview.jsx
+++ imports/ui/QuestionPreview.jsx
@@ -18,17 +18,36 @@ export default class QuestionPreview extends Component {
     }
   };
 
+  handleEdit(evt){
+
+
+  }
+
   render() {
    question = this.props.question;// ? this.props.questions : {text:'no question in db'} ;
    if(!question){
     return( <div className='container'>Loading...</div>);
    }
    else{
+    //Show delete button only if the question has an id (i.e. it's from the database, not the editor)
+    deleteButton = null;
+    editButton = null;
+    if (question._id){
+      deleteButton = <button type="button" className="btn btn-danger" onClick={this.handleDelete.bind(this)} >
+             <span className="glyphicon glyphicon-remove" ></span>
+             <br />
+           </button> ;
+      editButton = <button type="button" className="btn btn-info" onClick={this.handleEdit.bind(this)} >
+             <span className="glyphicon glyphicon-edit" ></span>
+             <br />
+           </button> ;
+
+    }
     return (
       <div className='panel panel-default'>
-        <div className="panel-heading"> 
+        <div className="panel-heading">
+           {deleteButton} {editButton} 
            Question: {this.props.question._id} <br />
-           <span className="glyphicon glyphicon-remove" onClick={this.handleDelete.bind(this)} ></span> 
            <HtmlView html={question.text} />
         </div>
         <div className="panel-body">
